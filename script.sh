98e5f5f6ee1184931620c2d34e23885238d23239
Marcus Ibsch <marcus.ibsch@gmail.com>
2017-01-22 19:30:58 +0100

    [BUGFIX] updates session name in tmux commands

--- script.sh
+++ script.sh
@@ -57,10 +57,10 @@ TMUX_SCRIPT=$(cat <<EOF
 tmux start-server
 
 tmux new-session -d -s selenium
-tmux send-keys -t selenium:0 './chromedriver' C-m
+tmux send-keys -t selenium './chromedriver' C-m
 
 tmux new-session -d -s chrome-driver
-tmux send-keys -t chrome-driver:0 'java -jar selenium-server-standalone.jar' C-m
+tmux send-keys -t chrome-driver 'java -jar selenium-server-standalone.jar' C-m
 EOF
 )
 echo "${TMUX_SCRIPT}"
